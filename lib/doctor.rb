0b39f331249c32c7820ec92bef4741458ec94ed7
Justin Davis <davisjustinw@gmail.com>
2019-01-17 07:15:22 +0000

    Automatically backed up by Learn

--- lib/doctor.rb
+++ lib/doctor.rb
@@ -1,5 +1,5 @@
 class Doctor
-  attr_accessor :name 
+  attr_accessor :name, :appointments 
   @@all = []
   
   def initialize(name)
@@ -7,5 +7,10 @@ class Doctor
     @@all << self
   end
   
+  def new_appointment(date, patient)
+    new_appointment = Appointment.new(date, self, patient)
+    @appointments << new_appointment
+    new_appointment
+  end
   
 end
