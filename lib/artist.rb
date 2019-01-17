7c651607d79cdf02e7bce1877d1e7d675d6053ee
Justin Davis <davisjustinw@gmail.com>
2019-01-17 06:56:56 +0000

    Automatically backed up by Learn

---
+++ lib/artist.rb
@@ -0,0 +1,28 @@
+require 'pry'
+class Artist 
+  attr_accessor :name, :songs
+  
+  @@all = []
+  def initialize(name)
+    @songs = []
+    @name = name 
+    @@all << self 
+  end 
+  
+  def self.all 
+    @@all
+  end 
+  
+  def new_song(name, genre)
+     
+    this_song = Song.new(name, self, genre)
+    @songs << this_song
+    
+  end
+  
+  def genres 
+    songs.collect { |song| song.genre }.uniq 
+  end 
+  
+end 
+binding.pry
